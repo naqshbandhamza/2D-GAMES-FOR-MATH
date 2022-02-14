4c5e3a6c14848a04cb102223590db9cf9a3bd4c4
Muhammad Hamza Latif <85412839+naqshbandhamza@users.noreply.github.com>
2022-02-14 18:57:34 +0500

    Add files via upload

--- public/game/godot-game-html/index.js
+++ public/game/godot-game-html/index.js
@@ -1,7 +1,7 @@
 
 var Godot = (function() {
   var _scriptDir = typeof document !== 'undefined' && document.currentScript ? document.currentScript.src : undefined;
-  
+
   return (
 function(Godot) {
   Godot = Godot || {};
